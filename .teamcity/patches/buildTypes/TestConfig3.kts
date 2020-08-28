4baf841f07800383694fa5faa63df91532e1b446
inna_yan <inna_yan@TeamCity>
2020-08-28 19:09:31 +0300

    TeamCity change in 'Successful flaky test' project: runners of 'Test config3' build configuration were updated

--- .teamcity/patches/buildTypes/TestConfig3.kts
+++ .teamcity/patches/buildTypes/TestConfig3.kts
@@ -50,11 +50,6 @@ changeBuildType(RelativeId("TestConfig3")) {
                     
                     echo "##teamcity[testStarted name='MyTest1.test8']"
                     echo "##teamcity[testFinished name='MyTest1.test8']"
-                    
-                    
-                    echo "##teamcity[testStarted name='MyTest1.test8']"
-                    echo "##teamcity[testFailed name='MyTest1.test8' message='failure message' details='message and stack trace']"
-                    echo "##teamcity[testFinished name='MyTest1.test8']"
                 """.trimIndent()
             }
         }
