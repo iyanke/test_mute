1eff5de41d5cc29ba59939eb5b98515349c951dd
inna_yan <inna_yan@TeamCity>
2020-10-18 12:01:56 +0300

    TeamCity change in 'Successful flaky test' project: 'Test config' build configuration settings were updated

--- .teamcity/patches/buildTypes/TestConfig.kts
+++ .teamcity/patches/buildTypes/TestConfig.kts
@@ -1,6 +1,7 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.ScriptBuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
 
@@ -120,6 +121,10 @@ changeBuildType(RelativeId("TestConfig")) {
         }
     }
     steps {
+        update<ScriptBuildStep>(4) {
+            enabled = false
+            clearConditions()
+        }
         insert(5) {
             script {
                 name = "1"
